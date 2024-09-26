5ffffba229942cb7e05efa7847c0865d41652041
GenaPlem <shainkill1337@gmail.com>
2024-09-26 13:20:16 +0100

    Add real data to MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,17 @@
 import '../index.css'
-import movieMockImg from '../assets/movie_mock_img.jpg'
+// import movieMockImg from '../assets/movie_mock_img.jpg'
 
-const MovieCard = () => {
+const MovieCard = ({title, poster_path, release_date}) => {
+    
     return (
-        <article className='overflow-hidden shadow-neon rounded-xl '>
+        <article className='flex flex-col justify-between overflow-hidden shadow-neon rounded-xl'>
             <div className=''>
-                <img src={movieMockImg} alt="It Ends With Us" className='h-full'/>
+                <img src={`https://image.tmdb.org/t/p/w200/${poster_path}`} alt={title} className='object-cover w-full h-auto'/>
             </div>
-            <h2 className='my-4 md:text-xl'>It Ends With Us</h2>
-            <p className='mb-5 text-slate-500'>Aug 08, 2024</p>
+        
+                <h2 className='my-2 text-sm font-bold md:text-xl'>{title}</h2>
+                <p className='mb-2 text-xs text-slate-500'>{release_date}</p>
+
         </article>
     )
 }
