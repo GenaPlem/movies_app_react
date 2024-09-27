7d33698c1a53550d5a6a3ca968f9c2af4331231e
GenaPlem <shainkill1337@gmail.com>
2024-09-27 11:00:23 +0100

    Add function to handle fetch request

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,25 +1,36 @@
-import '../index.css'
+import '../index.css';
 import MovieCard from './MovieCard';
+import Loader from './Loader';
 import { useEffect, useState } from 'react';
-import { useGetData } from '../api/movies';
+import { fetchData } from '../api/movies';
 
 const Movies = () => {
 
     const [movies, setMovies] = useState([]);
-    
-    useEffect(() => {
-        const useGetMovies = () => {
-            useGetData('popular', setMovies)
-        }
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-        useGetMovies()
+    const handleFetchData = async () => {
+        setLoading(true);
+        setError(null);
+
+        await fetchData('popular', setMovies);
+
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        handleFetchData()
     }, [])
 
+    if (loading) {
+        return <Loader/>
+    }
+
     return (
-        
         <main>
             <h1 className='text-3xl font-semibold my-9 md:text-5xl'>Your Ultimate Movie Guide</h1>
-            <section className='grid grid-cols-2 gap-10 mt-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
+            <section className='grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
                 {movies.length > 0 && movies.map(movie => <MovieCard {...movie} key={movie.id}/>)}
             </section>
         </main>
