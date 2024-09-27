a5c9d72d937af1526a5293c696c126557e0ab587
GenaPlem <shainkill1337@gmail.com>
2024-09-27 13:09:45 +0100

    Add function helper to formate date

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,5 @@
 import '../index.css'
-// import movieMockImg from '../assets/movie_mock_img.jpg'
+import { formateDate } from '../helpers/formateDate';
 
 const MovieCard = ({title, poster_path, release_date}) => {
     
@@ -9,7 +9,7 @@ const MovieCard = ({title, poster_path, release_date}) => {
                 <img src={`https://image.tmdb.org/t/p/w200/${poster_path}`} alt={`Poster of ${title} movie`} className='object-cover w-full h-auto'/>
             </div>
             <h2 className='my-2 text-sm font-bold md:text-xl'>{title}</h2>
-            <p className='mb-2 text-xs text-slate-500'>{release_date}</p>
+            <p className='mb-2 text-xs text-slate-500'>{formateDate(release_date)}</p>
             <a href="#" className='absolute inset-0 z-10' aria-label='Read more'></a>
         </article>
     )
