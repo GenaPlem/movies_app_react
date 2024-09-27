e85b540260f48c2b48d7eb9913854bb9866ea6e4
GenaPlem <shainkill1337@gmail.com>
2024-09-27 11:27:12 +0100

    Update Loader's size

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
     return (    
         <div className='flex flex-col items-center justify-center flex-1'>
             <l-trefoil
-                size="40"
+                size="65"
                 stroke="4"
                 stroke-length="0.15"
                 bg-opacity="0.1"
