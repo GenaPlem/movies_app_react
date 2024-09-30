909cac71a0ea4799d24f2e37a0648f1349df93fb
GenaPlem <shainkill1337@gmail.com>
2024-09-30 11:12:52 +0100

    Add Invoices to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Header from './components/Header.jsx'
-import Movies from './components/Movies.jsx'
-import Footer from './components/Footer.jsx'
 import './App.css'
+import Invoices from './pages/component.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
 
     return (
         <>
             <Header/>
-            <Movies/>
+            <Invoices/>
             <Footer/>
         </>
     )
