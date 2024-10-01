d09bb3f00b4b535763fd50373500dbde72fea1e3
GenaPlem <shainkill1337@gmail.com>
2024-10-01 14:01:12 +0100

    Add styles for Error page

--- src/routes/Error/component.jsx
+++ src/routes/Error/component.jsx
@@ -1,11 +1,12 @@
+import '../../index.css'
 import {Link} from "react-router-dom";
 
 const Error = () => (
-    <main>
-        <h2>Ooops, it's error innit?</h2>
-        <p><b>404</b></p>
+    <main className='flex flex-col justify-center'>
+        <h1 className='text-3xl md:text-5xl'>Oops, something went wrong</h1>
+        <p className='my-10 text-2xl font-bold'>404</p>
         <nav>
-            <Link to="/">Back to home</Link>
+            <Link to="/" className='text-xl'>Back to home</Link>
         </nav>
     </main>
 );
