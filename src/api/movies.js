10022e12f2ac56ce99ade4368b59434a76d72df9
GenaPlem <shainkill1337@gmail.com>
2024-10-01 13:59:31 +0100

    Update fetch api function

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.VITE_API_KEY
 
-export const fetchData = async (category, setData, setError) => {
+export const fetchData = async (request, setData, setError, page = 0) => {
 
     const options = {
         method: 'GET',
@@ -10,8 +10,10 @@ export const fetchData = async (category, setData, setError) => {
         }
       };
       
-      fetch(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`, options)
+      fetch(`https://api.themoviedb.org/3/movie/${request}?language=en-US${page?`&page=${page}`:''}`, options)
         .then(response => response.json())
-        .then(response => setData(response.results))
+        .then(data => {
+          data.results ? setData(data.results) : setData(data)
+        })
         .catch(err => setError(err));
 }
