bdfc479cfd49a5ca7961220e91dc78d8bf02492b
GenaPlem <shainkill1337@gmail.com>
2024-10-04 13:54:00 +0100

    Add handleSearch function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import searchIcon from '../assets/search_icon.svg';
 import { useState } from 'react';
 
 const SearchBar = () => {
     const [query, setQuery] = useState('');
+    const navigate = useNavigate();
 
     const handleChangeSearch = (e) => {
         setQuery(e.target.value)
+    }
+
+    const handleSearch = () => {
+        query && navigate(`/search/${query}`);          
     }
 
     return (
@@ -17,7 +23,7 @@ const SearchBar = () => {
                 placeholder="Search a movie..."
                 className="p-1.5 w-32 sm:w-full border-sky-400 border bg-inherit rounded-l"
             />
-            <button className='p-1.5 rounded-l-none rounded-r bg-sky-400'>
+            <button className='p-1.5 rounded-l-none rounded-r bg-sky-400' onClick={handleSearch}>
                 <img src={searchIcon} alt="Search Icon" className='size-4 sm:size-5' />
             </button>
         </div>
