3d9908a3966999f99c98b75a2df7401339fa5049
GenaPlem <shainkill1337@gmail.com>
2024-10-04 12:13:25 +0100

    Update SearchBar styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,21 @@
 import searchIcon from '../assets/search_icon.svg';
+import { useState } from 'react';
 
 const SearchBar = () => {
     const [query, setQuery] = useState('');
 
     return (
-        <>
+        <div className='flex justify-center'>
             <input 
                 type="text"
                 value={query}
                 placeholder="Search for a movie..."
-                className="p-2 border rounded"
+                className="p-1.5 border-sky-400 border bg-inherit rounded-l"
             />
-            <button className='p-2 ml-2 rounded bg-sky-400'>
-                {searchIcon}
+            <button className='p-1.5 rounded-l-none rounded-r bg-sky-400'>
+                <img src={searchIcon} alt="Search Icon" className='size-5' />
             </button>
-        </>
+        </div>
     )
 }
 
