a36c8d7b562141110c53a29c796deb8c817468c4
GenaPlem <shainkill1337@gmail.com>
2024-10-04 11:59:34 +0100

    Add SearchBar to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import '../index.css';
 import logo from '../assets/react.svg';
 import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
 const Header = () => {
 
     return (
         <header className='flex justify-between mb-5 md:mb-10'>
             <Link to="/"><img src={logo} alt="logo" className='logo'/></Link>
-            <ul className='flex gap-10'>
+            <ul className='flex items-center gap-10'>
                 <li><a href="#" className='text-white hover:text-sky-400'>All</a></li>
                 <li><a href="#" className='text-white hover:text-sky-400'>Popular</a></li>
-                <li><a href="#" className='text-white hover:text-sky-400'>New</a></li>
+                <li><SearchBar /></li>
             </ul>
         </header>
     );
