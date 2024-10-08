f044e36ed4410f9eab8f533254ec1f6a5a06e6cd
GenaPlem <shainkill1337@gmail.com>
2024-10-08 12:53:58 +0100

    Add 'Load more' button and functionality

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -8,18 +8,27 @@ const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         fetchData('popular', '', setMovies, setError);
         setLoading(false);
     }, []);
 
+    const handleLoadMoreClick = () => {
+        setPage(prevPage => prevPage + 1);
+        fetchData('popular', '', setMovies, setError, page + 1, true);
+    }
+
     if (loading) return <Loader/>
     if (error) return <p>Error: {error}</p>
 
     return (
-        <section className='grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
-            {movies.length > 0 && movies.map(movie => <MovieCard {...movie} key={movie.id}/>)}
+        <section>
+            <div className='grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
+                {movies.length > 0 && movies.map(movie => <MovieCard {...movie} key={movie.id}/>)}
+            </div>
+            <button className='text-black bg-sky-400' onClick={handleLoadMoreClick}>Load more</button>
         </section>
     );
 };
