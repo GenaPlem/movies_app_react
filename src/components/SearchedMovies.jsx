41d9184bd1ae0c8bd36b67f070650181f938a778
GenaPlem <shainkill1337@gmail.com>
2024-10-08 11:55:44 +0100

    Update SearchedMovies component with data

--- src/components/SearchedMovies.jsx
+++ src/components/SearchedMovies.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { fetchData } from "../api/movies";
+import Loader from "./Loader";
+import SearchedMovie from "./SearchedMovie";
 
 const SearchedMovies = () => {
     const {query} = useParams();
+    const [searchResults, setSearchResults] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchData('search', query, setSearchResults, setError);
+        setLoading(false);
+    }, [query]);
+
+    if (loading) return <Loader/>
+    if (error) return <p>Error: {error}</p>
 
     return (
         <main>
-            {query}
+            <section className='flex flex-col gap-10'>
+                <h1 className="mt-3 text-2xl">Movies searched by: {query}</h1>
+                {searchResults.length > 0 ?
+                 searchResults.map(movie => <SearchedMovie {...movie} key={movie.id}/>) :
+                 <p>No results found</p>
+                 }
+            </section>
         </main>
     )
 
