1606140a5302b642c9cf8042b7943fff4ba39d57
GenaPlem <shainkill1337@gmail.com>
2024-10-08 15:04:57 +0100

    Update title styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ const MovieCard = ({id, title, poster_path, release_date}) => {
         <article className='relative flex flex-col justify-between overflow-hidden transition duration-300 transform shadow-neon rounded-xl hover:scale-105'>
             <div>
                 <img src={`https://image.tmdb.org/t/p/w200/${poster_path}`} alt={`Poster of ${title} movie`} className='object-cover w-full h-auto'/>
-                <h2 className='m-1 text-sm font-bold md:text-xl'>{title}</h2>
+                <h2 className='m-1 text-sm font-bold md:text-xl line-clamp-2'>{title}</h2>
             </div>
             <p className='mb-2 text-xs text-slate-500'>{formateDate(release_date)}</p>
             <Link to={`/details/${id}`} className='absolute inset-0 z-10' aria-label='Read more' />
