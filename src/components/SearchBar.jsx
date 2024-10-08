a008be77bc4f8b2e36daa99050789953254382a4
GenaPlem <shainkill1337@gmail.com>
2024-10-08 11:03:48 +0100

    Update SearchBar using form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,12 +10,13 @@ const SearchBar = () => {
         setQuery(e.target.value)
     }
 
-    const handleSearch = () => {
+    const handleSearch = (e) => {
+        e.preventDefault();
         query && navigate(`/search/${query}`);          
     }
 
     return (
-        <div className='flex justify-center'>
+        <form onSubmit={handleSearch} className='flex justify-center'>
             <input 
                 type="text"
                 value={query}
@@ -23,10 +24,10 @@ const SearchBar = () => {
                 placeholder="Search a movie..."
                 className="p-1.5 w-32 sm:w-full border-sky-400 border bg-inherit rounded-l"
             />
-            <button className='p-1.5 rounded-l-none rounded-r bg-sky-400' onClick={handleSearch}>
+            <button type='submit' className='p-1.5 rounded-l-none rounded-r bg-sky-400'>
                 <img src={searchIcon} alt="Search Icon" className='size-4 sm:size-5' />
             </button>
-        </div>
+        </form>
     )
 }
 
