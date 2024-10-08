4b2f0050a44055585d2dae5b575ea866820a07d4
GenaPlem <shainkill1337@gmail.com>
2024-10-08 15:10:47 +0100

    Add font-size for title

--- src/components/SearchedMovie.jsx
+++ src/components/SearchedMovie.jsx
@@ -9,10 +9,10 @@ const SearchedMovie = ({id, title, poster_path, release_date, overview}) => {
                 <img src={`https://image.tmdb.org/t/p/w200/${poster_path}`} alt={`Poster of ${title} movie`} className='object-cover h-auto w-28 md:w-full'/>
             </div>
             <div className='flex flex-col flex-1'>
-                <h2 className='mt-2 text-sm font-bold md:text-xl line-clamp-1'>{title}</h2>
+                <h2 className='mt-2 font-bold text-md md:text-xl line-clamp-1'>{title}</h2>
                 <p className='mb-2 text-xs text-slate-500'>{formateDate(release_date)}</p>
                 <Link to={`/details/${id}`} className='absolute inset-0 z-10' aria-label='Read more' />
-                <p className='mx-2 mt-5 text-sm text-start md:mx-3 line-clamp-3 md:line-clamp-6'>{overview}</p>
+                <p className='mx-2 mt-5 text-sm md:text-lg text-start md:mx-3 line-clamp-3 md:line-clamp-6'>{overview}</p>
             </div>
         </article>
     )
