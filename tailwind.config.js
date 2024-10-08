e80d36246bb99f7262ff8c526f82c93a2e63dc6c
GenaPlem <shainkill1337@gmail.com>
2024-10-08 15:04:39 +0100

    Add lineClamp for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import lineClamp from '@tailwindcss/line-clamp';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -11,5 +13,7 @@ export default {
             }
         },
     },
-    plugins: [],
+    plugins: [
+        lineClamp,
+      ],
 }
