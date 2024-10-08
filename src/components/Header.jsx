5289fb540b342acfb654ee6a78321e5a8eb9b81a
GenaPlem <shainkill1337@gmail.com>
2024-10-08 11:04:01 +0100

    Update logo styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
     return (
         <header className='flex justify-between mb-5 md:mb-10'>
-            <Link to="/"><img src={logo} alt="logo" className='logo'/></Link>
+            <Link to="/"><img src={logo} alt="logo" className='logo size-8 sm:size-10'/></Link>
             <ul className='flex items-center gap-3 text-sm sm:text-base sm:gap-10'>
                 <li><a href="#" className='text-white hover:text-sky-400'>All</a></li>
                 <li><a href="#" className='text-white hover:text-sky-400'>Popular</a></li>
