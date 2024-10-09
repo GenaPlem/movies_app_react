253521fb865b63a88d83f92feda8167dc486dcd4
GenaPlem <shainkill1337@gmail.com>
2024-10-09 10:21:27 +0100

    Update navigation in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,7 @@ const Header = () => {
         <header className='flex justify-between mb-5 md:mb-10'>
             <Link to="/"><img src={logo} alt="logo" className='logo size-8 sm:size-10'/></Link>
             <ul className='flex items-center gap-3 text-sm sm:text-base sm:gap-10'>
-                <li><a href="#" className='text-white hover:text-sky-400'>All</a></li>
-                <li><a href="#" className='text-white hover:text-sky-400'>Popular</a></li>
+                <li><Link to="/" className='text-white hover:text-sky-400'>All</Link></li>
                 <li><SearchBar /></li>
             </ul>
         </header>
