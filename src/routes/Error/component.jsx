02995e6d81f177fea567b0670795c0c547f9e322
GenaPlem <shainkill1337@gmail.com>
2024-10-16 14:29:29 +0100

    Add RootLayout component

--- src/routes/Error/component.jsx
+++ src/routes/Error/component.jsx
@@ -1,14 +1,16 @@
-import '../../index.css'
-import {Link} from "react-router-dom";
+import "../../index.css";
+import { Link } from "react-router-dom";
 
 const Error = () => (
-    <main className='flex flex-col justify-center'>
-        <h1 className='text-3xl md:text-5xl'>Oops, something went wrong</h1>
-        <p className='my-10 text-2xl font-bold'>404</p>
-        <nav>
-            <Link to="/" className='text-xl'>Back to home</Link>
-        </nav>
-    </main>
+  <main className="flex flex-col justify-center">
+    <h1 className="text-3xl md:text-5xl">Oops, something went wrong</h1>
+    <p className="my-10 text-2xl font-bold">404</p>
+    <nav>
+      <Link to="/" className="text-xl">
+        Back to home
+      </Link>
+    </nav>
+  </main>
 );
 
 export default Error;
