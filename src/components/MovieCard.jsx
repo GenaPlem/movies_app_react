624a29ce8de0ac8401a6d0557ce2bb5ae162172d
GenaPlem <shainkill1337@gmail.com>
2024-10-16 11:46:15 +0100

    Update isLoading appearance

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,25 +1,38 @@
-import { Link } from 'react-router-dom';
-import { formateDate } from '../helpers/utils';
-import movie_mock_img from '../assets/movie_mock_img.png';
+import { Link } from "react-router-dom";
+import { formateDate } from "../helpers/utils";
+import movie_mock_img from "../assets/movie_mock_img.png";
 
-const MovieCard = ({id, title, poster_path, release_date}) => {
+const MovieCard = ({ id, title, poster_path, release_date }) => {
+  let movieImg = "";
 
-    let movieImg = '';
+  poster_path
+    ? (movieImg = `https://image.tmdb.org/t/p/w200/${poster_path}`)
+    : (movieImg = movie_mock_img);
 
-    poster_path ? 
-    movieImg = `https://image.tmdb.org/t/p/w200/${poster_path}` :
-    movieImg = movie_mock_img;
-    
-    return (
-        <article className='relative flex flex-col justify-between overflow-hidden transition duration-300 transform shadow-neon rounded-xl hover:scale-105'>
-            <div>
-                <img src={movieImg} alt={`Poster of ${title} movie`} className='object-cover w-full h-auto'/>
-                <h2 className='m-1 text-sm font-bold md:text-xl line-clamp-2'>{title}</h2>
-            </div>
-            <p className='mb-2 text-xs text-slate-500'>{formateDate(release_date)}</p>
-            <Link to={`/details/${id}`} className='absolute inset-0 z-10' aria-label='Read more' />
-        </article>
-    )
-}
+  return (
+    <article className="relative flex flex-col justify-between overflow-hidden transition duration-300 transform shadow-neon rounded-xl hover:scale-105">
+      <div>
+        <img
+          src={movieImg}
+          alt={`Poster of ${title} movie`}
+          className="object-cover w-full h-auto"
+        />
+        <h2 className="m-1 text-sm font-bold md:text-xl line-clamp-2">
+          {title}
+        </h2>
+      </div>
+      {release_date && (
+        <p className="mb-2 text-xs text-slate-500">
+          {formateDate(release_date)}
+        </p>
+      )}
+      <Link
+        to={`/details/${id}`}
+        className="absolute inset-0 z-10"
+        aria-label="Read more"
+      />
+    </article>
+  );
+};
 
 export default MovieCard;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,36 +1,55 @@
-import MovieCard from './MovieCard';
-import Loader from './Loader';
-import { useEffect, useState } from 'react';
-import { fetchData } from '../api/movies';
+import MovieCard from "./MovieCard";
+import Loader from "./Loader";
+import { useEffect, useState } from "react";
+import { fetchData } from "../api/movies";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadMore, setIsLoadMore] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
 
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [page, setPage] = useState(1);
+  useEffect(() => {
+    setIsLoading(true);
+    fetchData("popular", "", setMovies, setError);
+    setIsLoading(false);
+  }, []);
 
-    useEffect(() => {
-        fetchData('popular', '', setMovies, setError);
-        setLoading(false);
-    }, []);
+  const handleLoadMoreClick = () => {
+    setIsLoadMore(true);
+    setPage((prevPage) => prevPage + 1);
+    fetchData("popular", "", setMovies, setError, page + 1, true);
+    setIsLoadMore(false);
+  };
 
-    const handleLoadMoreClick = () => {
-        setPage(prevPage => prevPage + 1);
-        fetchData('popular', '', setMovies, setError, page + 1, true);
-    }
+  if (error) return <p>Error: {error}</p>;
 
-    if (loading) return <Loader/>
-    if (error) return <p>Error: {error}</p>
-
-    return (
-        <section>
-            <div className='grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
-                {movies.length > 0 && movies.map(movie => <MovieCard {...movie} key={movie.id}/>)}
-            </div>
-            <button className='text-black bg-sky-400' onClick={handleLoadMoreClick}>Load more</button>
-        </section>
-    );
+  return (
+    <section>
+      {!isLoading && movies.length > 0 && (
+        <>
+          <div className="grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+            {movies.map((movie) => (
+              <MovieCard {...movie} key={movie.id} />
+            ))}
+          </div>
+          <button
+            className="text-black bg-sky-400"
+            onClick={handleLoadMoreClick}
+            disabled={isLoadMore}
+          >
+            Load more
+          </button>
+        </>
+      )}
+      {isLoading && (
+        <div>
+          <Loader />
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default Movies;
