7980c364c265952b4a10590e80f38623f61ab7ab
GenaPlem <shainkill1337@gmail.com>
2024-10-16 11:00:44 +0100

    Add main into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import Invoices from './pages/component.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import "./App.css";
+import Invoices from "./pages/component.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-
-    return (
-        <>
-            <Header/>
-            <Invoices/>
-            <Footer/>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <main>
+        <Invoices />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
