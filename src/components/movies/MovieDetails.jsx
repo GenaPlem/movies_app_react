f94c1430ef4f0cd92548a24451635dc8c21bba2d
GenaPlem <shainkill1337@gmail.com>
2024-10-17 11:38:05 +0100

    Add smooth backdrop img loading

--- src/components/movies/MovieDetails.jsx
+++ src/components/movies/MovieDetails.jsx
@@ -1,21 +1,33 @@
 import { useLoaderData } from "react-router-dom";
 import { calculateRate } from "../../helpers/utils";
 import backdrop_mock_img from "../../assets/backdrop_mock_img.png";
+import { useState } from "react";
 
 const MovieDetails = () => {
   const movie = useLoaderData();
+  const [imgLoaded, setImgLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImgLoaded(true);
+  };
 
   return (
     <>
-      {movie.backdrop_path ? (
+      {!imgLoaded && (
         <img
-          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-          alt={movie.title}
-          // loading="lazy"
+          src={backdrop_mock_img}
+          alt="Loading placeholder"
+          className="opacity-30"
+          style={{ filter: "blur(10px)", transition: "0.5s" }}
         />
-      ) : (
-        <img src={backdrop_mock_img} alt={movie.title} className="opacity-30" />
       )}
+      <img
+        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        alt={movie.title}
+        onLoad={handleImageLoad}
+        style={{ display: imgLoaded ? "block" : "none" }}
+      />
+
       <h1 className="my-10 text-2xl sm:text-3xl">{movie.title}</h1>
       <div className="flex items-start my-5">
         {movie.vote_average > 0 && (
@@ -25,8 +37,7 @@ const MovieDetails = () => {
           </p>
         )}
         <div className="flex flex-wrap items-center justify-end gap-5 ml-auto">
-          {movie.genres &&
-            movie.genres.length > 0 &&
+          {movie.genres.length > 0 &&
             movie.genres.map((genre) => (
               <p
                 className="block p-1 text-xs text-black rounded-md sm:text-sm bg-sky-400"
