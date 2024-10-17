14c7edc2e8da81c339107ffc74a1d7c333e4c54f
GenaPlem <shainkill1337@gmail.com>
2024-10-17 13:15:21 +0100

    Update footer styles

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return (
-        <footer className='flex justify-between pt-10 pb-1 text-gray-500'>
-            <p className='mx-auto'>&copy; {new Date().getFullYear()}  MovieApp. All rights reserved.</p>
-        </footer>
-    )
-}
+  return (
+    <footer className="flex justify-between pt-10 pb-1 mt-6 text-gray-500">
+      <p className="mx-auto">
+        &copy; {new Date().getFullYear()} MovieApp. All rights reserved.
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
