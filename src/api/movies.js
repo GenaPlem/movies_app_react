be9dacb15780319de1cc7d025949d3571783a143
GenaPlem <shainkill1337@gmail.com>
2024-10-17 11:06:45 +0100

    Add fetch popular movies function

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,4 +1,13 @@
-const API_KEY = import.meta.env.VITE_API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
 
 export const fetchData = async (type, queryOrId = '', setData, setError, page = 1, loadMore = false) => {
 
@@ -41,4 +50,12 @@ export const fetchData = async (type, queryOrId = '', setData, setError, page = 
     } catch (err) {
       setError(err.message)
     }
+}
+
+export const fetchPopularMovies = async (page = 1) => {
+  const response = await fetch(`${BASE_URL}/movie/popular?language=en-US&page=${page}`, options)
+  if (!response.ok) {
+    throw new Error('Failed to fetch popular movies.');
+  }
+  return response.json();
 }
