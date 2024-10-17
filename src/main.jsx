307eddf5ca05bfb789c1b18014ca5b0fadcc2775
GenaPlem <shainkill1337@gmail.com>
2024-10-17 16:42:37 +0100

    Add query client provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import "./App.css";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
   </StrictMode>
 );
