6e5f1cde38fd07544eb370d7245de41fd667c872
GenaPlem <shainkill1337@gmail.com>
2024-10-17 11:07:19 +0100

    Remove useEffect fetch data by using loader

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,33 +1,36 @@
 import MovieCard from "./MovieCard";
-import Loader from "../Loader";
-import { useEffect, useState } from "react";
-import { fetchData } from "../../api/movies";
+import { useState } from "react";
+import { fetchPopularMovies } from "../../api/movies";
+import { useLoaderData } from "react-router-dom";
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
+  const initialData = useLoaderData();
+  const [movies, setMovies] = useState(initialData.results || []);
   const [isLoading, setIsLoading] = useState(false);
-  const [isLoadMore, setIsLoadMore] = useState(false);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
 
-  useEffect(() => {
+  const handleLoadMoreClick = async () => {
     setIsLoading(true);
-    fetchData("popular", "", setMovies, setError);
-    setIsLoading(false);
-  }, []);
-
-  const handleLoadMoreClick = () => {
-    setIsLoadMore(true);
     setPage((prevPage) => prevPage + 1);
-    fetchData("popular", "", setMovies, setError, page + 1, true);
-    setIsLoadMore(false);
+    try {
+      const data = await fetchPopularMovies(page + 1);
+      const uniqueMovies = data.results.filter(
+        (movie) => !movies.find((m) => m.id === movie.id)
+      );
+      setMovies((prevMovies) => [...prevMovies, ...uniqueMovies]);
+    } catch (err) {
+      setError("Failed to load more movies");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   if (error) return <p>Error: {error}</p>;
 
   return (
     <>
-      {!isLoading && movies.length > 0 && (
+      {movies.length > 0 && (
         <section>
           <div className="grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             {movies.map((movie) => (
@@ -37,16 +40,11 @@ const Movies = () => {
           <button
             className="text-black bg-sky-400"
             onClick={handleLoadMoreClick}
-            disabled={isLoadMore}
+            disabled={isLoading}
           >
-            {isLoadMore ? "Loading..." : "Load more"}
+            {isLoading ? "Loading..." : "Load more"}
           </button>
         </section>
-      )}
-      {isLoading && (
-        <div className="flex items-center justify-center flex-1">
-          <Loader />
-        </div>
       )}
     </>
   );
